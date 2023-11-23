02d67a9bf10660f3d23ed229dab66533e4a35cb1
Lê Đức Anh <leanhthuongx7@gmail.com>
2023-11-23 13:54:46 +0700

    23-11-2023

--- js/app.js
+++ js/app.js
@@ -14,11 +14,26 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-$(document).ready(function(){
-    $('#menu-icon').click(function(){
+$(document).ready(function () {
+    $('#menu-icon').click(function () {
         $('.main-menu').slideToggle();
     })
+
+
 })
+
+function FallinOut() {
+    var x = document.getElementById("accordion-inner");
+    if (x.style.display == "block") {
+
+        $('#accordion-inner').slideToggle();
+        $('.accordion-title').removeClass('active');
+    } else {
+        $('#accordion-inner').slideToggle();
+        $('.accordion-title').addClass('active');
+    }
+
+}
 
 
 
